f7554ff83b71a834269fa5673f3800f9f98b96bb
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 02:16:05 +0700

    Update mult.py

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,1 +1,10 @@
-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+result = num1 * num2
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
+print("The result of the multiplication is:", result)
