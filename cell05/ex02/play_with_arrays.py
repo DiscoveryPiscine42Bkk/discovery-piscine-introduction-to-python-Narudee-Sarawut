dd9a6ab9812374ffaa019ce40e03263952b2be59
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 04:36:03 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,1 +1,7 @@
-
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+for number in original_array:
+    if number > 5:
+        new_array.append(number + 2)
+print("Original array:", original_array)
+print("Processed array (values > 5, each +2):", new_array)
