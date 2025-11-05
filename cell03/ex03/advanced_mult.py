c19e4c18a79956f85d78ffbdb5844be04cb8934f
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 02:45:49 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,1 +1,9 @@
-
+table = 1
+while table <= 10:
+    print(f"\nMultiplication table for {table}:")  
+    multiplier = 1
+    while multiplier <= 10:
+        result = table * multiplier
+        print(f"{table} x {multiplier} = {result}")
+        multiplier += 1
+    table += 1
