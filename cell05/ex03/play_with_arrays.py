f0d1c2c775bc31fe922da7360a6d4be9dc177525
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 04:38:41 +0700

    Update play_with_arrays.py

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,1 +1,9 @@
-
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+for number in original_array:
+    if number > 5:
+        value = number + 2
+        if value not in new_array:
+            new_array.append(value)
+print("Original array:", original_array)
+print("Processed array (values > 5, +2, no duplicates):", new_array)
