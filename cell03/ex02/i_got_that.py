54b849e52b466d2d441f9814b7a184bfe721dc5a
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 02:42:21 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,1 +1,7 @@
-
+while True:
+    user_input = input("Enter something (type STOP to end): ")
+    if user_input == "STOP":
+        print("Program stopped.")
+        break
+    else:
+        print(f"You entered: {user_input}")
