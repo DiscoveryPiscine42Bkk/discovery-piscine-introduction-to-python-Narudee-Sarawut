8918ba6324f6d79e42bb7a1d29f0d94a4be7f564
Narudee-Sarawut <67160016@kmitl.ac.th>
2025-11-06 19:53:48 +0700

    Update parameter_matching.py

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,10 @@
-
+import sys 
+if len(sys.argv) == 2:
+    keyword = sys.argv[1] 
+    user_word = input("Enter a word: ")
+    if user_word == keyword:
+        print("Good job!", "\n")
+    else:
+        print("Nope, sorry...", "\n")
+else:
+    print("none", "\n")
